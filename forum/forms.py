a9821d55ee5bc54aa29ddbc014b19ff4cd6ec3c8
Ckpn <ckapan89@gmail.com>
2024-05-01 16:57:37 +0300

    Commit message

--- forum/forms.py
+++ forum/forms.py
@@ -9,5 +9,5 @@ class CommentForm(ModelForm):
         def __init__(self, *args, **kwargs):
             super(CommentForm, self).__init__(*args,**kwargs)
             #self.fields['name'].widget.attrs.update({'class': 'form-control'})
-            for title, field in self.fields.items():
+            for  field in self.fields.values():
                 field.widget.attrs.update({'class':'form-control'})
