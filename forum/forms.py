f537d8d36cf04d155e5659aa63cf7ea6cdcee578
Ckpn <ckapan89@gmail.com>
2024-05-03 15:36:29 +0300

    Commit message

--- forum/forms.py
+++ forum/forms.py
@@ -6,8 +6,7 @@ class CommentForm(ModelForm):
         model = Comment
         fields = ['title','content','image']
         
-        def __init__(self, *args, **kwargs):
-            super(CommentForm, self).__init__(*args,**kwargs)
-            #self.fields['name'].widget.attrs.update({'class': 'form-control'})
-            for  field in self.fields.values():
-                field.widget.attrs.update({'class':'form-control'})
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args,**kwargs)
+        for title, field in self.fields.items():
+            field.widget.attrs.update({'class':'form-control mt-3', 'placeholder': field.label})
